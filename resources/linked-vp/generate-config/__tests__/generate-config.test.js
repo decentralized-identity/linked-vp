afddb956ae7894c4855eb7df929e0bae750a2946
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2024-02-08 17:41:42 +0100

    fix(examples): use JWT VP and VC, instead of mixing different formats

--- resources/linked-vp/generate-config/__tests__/generate-config.test.js
+++ resources/linked-vp/generate-config/__tests__/generate-config.test.js
@@ -12,7 +12,6 @@ const { documentLoader } = require("../services/documentLoader");
 const k0 = require("../keys/did-key-ed25519.json");
 
 let vpJwt;
-let verifiableCredential;
 let verifiablePresentation;
 
 it("can issue / verify linked data", async () => {
@@ -38,7 +37,7 @@ it("can issue / verify linked data", async () => {
       },
     },
   };
-  verifiableCredential = await vcjs.ld.createVerifiableCredential({
+  const verifiableCredential = await vcjs.ld.createVerifiableCredential({
     credential: credentialTemplate,
     suite: new Ed25519Signature2018({
       key,
@@ -100,29 +99,72 @@ it("can issue / verify linked data", async () => {
 it("can issue and verify jwt", async () => {
   const issuedDate = moment();
   const expirationDate = moment().add(5, "years");
-  const payload = {
+  const credentialTemplate = {
+    "@context": ["https://www.w3.org/2018/credentials/v1", { schema: "https://schema.org/" }],
+    issuer: k0.controller,
+    issuanceDate: moment().format(),
+    expirationDate: moment().add(5, "years").format(),
+    type: ["VerifiableCredential", "schema:Organization"],
+    credentialSubject: {
+      id: k0.controller,
+      "schema:legalName": "Example LLC",
+      "schema:telephone": "+1 23456 789",
+      "schema:taxID": "123456789",
+      "schema:location": {
+        "@type": " PostalAddress",
+        "schema:addressCountry": "Example Country",
+        "schema:addressRegion": "Example Region",
+        "schema:addressLocality": "Example City",
+        "schema:postalCode": "12345",
+        "schema:streetAddress": "1 Example Street",
+      },
+    },
+  };
+  const payloadVC = {
     sub: k0.controller,
     iss: k0.controller,
     nbf: issuedDate.unix(),
     iat: issuedDate.unix(),
     exp: expirationDate.unix(),
-    ...{
-      "@context": [
-        "https://www.w3.org/2018/credentials/v1",
-      ],
-      holder: k0.controller,
-      type: ["VerifiablePresentation"],
-      verifiableCredential: [verifiableCredential],
-      id: "https://bar.example.com/verifiable-presentation.jsonld",
-    },
+    // To be debated whether to use or not use the vp/vc properties as detailed in VC Data Model 2.0 and https://www.w3.org/TR/vc-jose-cose/#jose-header-parameters-and-jwt-claims. See also https://github.com/decentralized-identity/linked-vp/issues/15#issuecomment-1866203513
+    // ...credentialTemplate,
+    vc: credentialTemplate,
   };
-  const header = {
+  const headerVC = {
+    alg: "EdDSA",
+    kid: k0.id,
+    typ: "'vc+ld+jwt'",
+  };
+  const vcJwt = await new jose.CompactSign(Buffer.from(JSON.stringify(payloadVC)))
+    .setProtectedHeader(headerVC)
+    .sign(await jose.importJWK(k0.privateKeyJwk));
+  expect(vcJwt).toBeDefined();
+  const presentationTemplate = {
+    "@context": [
+      "https://www.w3.org/2018/credentials/v1",
+    ],
+    holder: k0.controller,
+    type: ["VerifiablePresentation"],
+    verifiableCredential: [vcJwt],
+    id: "https://bar.example.com/verifiable-presentation.jsonld",
+  };
+  const payloadVP = {
+    sub: k0.controller,
+    iss: k0.controller,
+    nbf: issuedDate.unix(),
+    iat: issuedDate.unix(),
+    exp: expirationDate.unix(),
+    // To be debated whether to use or not use the vp/vc properties as detailed in VC Data Model 2.0 and https://www.w3.org/TR/vc-jose-cose/#jose-header-parameters-and-jwt-claims. See also https://github.com/decentralized-identity/linked-vp/issues/15#issuecomment-1866203513
+    // ...presentationTemplate,
+    vp: presentationTemplate,
+  };
+  const headerVP = {
     alg: "EdDSA",
     kid: k0.id,
     typ: "'vp+ld+jwt'",
   };
-  vpJwt = await new jose.CompactSign(Buffer.from(JSON.stringify(payload)))
-    .setProtectedHeader(header)
+  vpJwt = await new jose.CompactSign(Buffer.from(JSON.stringify(payloadVP)))
+    .setProtectedHeader(headerVP)
     .sign(await jose.importJWK(k0.privateKeyJwk));
   // TODO: how to verify it?
   expect(vpJwt).toBeDefined();
